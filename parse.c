d17e9b94152213e9e8c8971492c8ffd6711094a9
navenlaya <navenchristian.laya@gmail.com>
2026-04-20 11:06:04 -0700

    add error messages for malformed input

--- parse.c
+++ parse.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include "parse.h"
@@ -15,12 +16,10 @@ int parse_input(char *input, pipeline_t *pl)
     pl->num_cmds = 0;
     pl->background = 0;
 
-    /* strip trailing newline */
     char *nl = strchr(input, '\n');
     if (nl)
         *nl = '\0';
 
-    /* skip leading whitespace */
     while (*input == ' ' || *input == '\t')
         input++;
     if (*input == '\0')
@@ -33,27 +32,39 @@ int parse_input(char *input, pipeline_t *pl)
     char *tok = strtok(input, " \t");
     while (tok) {
         if (strcmp(tok, "|") == 0) {
+            if (ai == 0) {
+                fprintf(stderr, "mysh: syntax error near '|'\n");
+                return -1;
+            }
             pl->cmds[ci].argv[ai] = NULL;
             ci++;
-            if (ci >= MAX_CMDS)
+            if (ci >= MAX_CMDS) {
+                fprintf(stderr, "mysh: too many piped commands\n");
                 return -1;
+            }
             init_command(&pl->cmds[ci]);
             ai = 0;
         } else if (strcmp(tok, "<") == 0) {
             tok = strtok(NULL, " \t");
-            if (!tok)
+            if (!tok) {
+                fprintf(stderr, "mysh: syntax error near '<'\n");
                 return -1;
+            }
             pl->cmds[ci].infile = tok;
         } else if (strcmp(tok, ">>") == 0) {
             tok = strtok(NULL, " \t");
-            if (!tok)
+            if (!tok) {
+                fprintf(stderr, "mysh: syntax error near '>>'\n");
                 return -1;
+            }
             pl->cmds[ci].outfile = tok;
             pl->cmds[ci].append = 1;
         } else if (strcmp(tok, ">") == 0) {
             tok = strtok(NULL, " \t");
-            if (!tok)
+            if (!tok) {
+                fprintf(stderr, "mysh: syntax error near '>'\n");
                 return -1;
+            }
             pl->cmds[ci].outfile = tok;
         } else if (strcmp(tok, "&") == 0) {
             pl->background = 1;
@@ -65,8 +76,13 @@ int parse_input(char *input, pipeline_t *pl)
     }
 
     pl->cmds[ci].argv[ai] = NULL;
+
     if (ai == 0 && ci == 0)
         return -1;
+    if (ai == 0 && ci > 0) {
+        fprintf(stderr, "mysh: syntax error near '|'\n");
+        return -1;
+    }
 
     pl->num_cmds = ci + 1;
     return 0;
