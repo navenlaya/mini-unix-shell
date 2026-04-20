fd4caa20d1e3ef10379d7b97157e154ac18206a7
navenlaya <navenchristian.laya@gmail.com>
2026-04-20 11:29:08 -0700

    add exit code tracking, SIGINT handling, and overlong input check

--- main.c
+++ main.c
@@ -7,8 +7,17 @@
 #include "parse.h"
 #include "execute.h"
 #include "builtins.h"
+#include "history.h"
 
 #define INPUT_SIZE 1024
+
+static volatile sig_atomic_t got_sigint;
+
+static void sigint_handler(int sig)
+{
+    (void)sig;
+    got_sigint = 1;
+}
 
 static void sigchld_handler(int sig)
 {
@@ -20,33 +29,70 @@ static void sigchld_handler(int sig)
 static void setup_signals(void)
 {
     struct sigaction sa;
-
-    /* ignore SIGINT and SIGTSTP in the shell process */
-    sa.sa_handler = SIG_IGN;
     sigemptyset(&sa.sa_mask);
+
+    sa.sa_handler = SIG_IGN;
     sa.sa_flags = 0;
-    sigaction(SIGINT, &sa, NULL);
     sigaction(SIGTSTP, &sa, NULL);
 
-    /* reap background children automatically */
+    sa.sa_handler = sigint_handler;
+    sa.sa_flags = 0;
+    sigaction(SIGINT, &sa, NULL);
+
     sa.sa_handler = sigchld_handler;
     sa.sa_flags = SA_RESTART | SA_NOCLDSTOP;
     sigaction(SIGCHLD, &sa, NULL);
+}
+
+static void drain_overlong(void)
+{
+    int c;
+    while ((c = fgetc(stdin)) != EOF && c != '\n')
+        ;
 }
 
 int main(void)
 {
     char input[INPUT_SIZE];
     pipeline_t pl;
+    history_t hist;
+    int last_status = 0;
 
+    history_init(&hist);
     setup_signals();
 
     for (;;) {
+        got_sigint = 0;
         printf("mysh> ");
         fflush(stdout);
 
-        if (!fgets(input, sizeof(input), stdin))
+        if (!fgets(input, sizeof(input), stdin)) {
+            if (got_sigint) {
+                clearerr(stdin);
+                printf("\n");
+                continue;
+            }
             break;
+        }
+
+        if (got_sigint) {
+            printf("\n");
+            continue;
+        }
+
+        if (!strchr(input, '\n') && !feof(stdin)) {
+            fprintf(stderr, "mysh: input too long\n");
+            drain_overlong();
+            continue;
+        }
+
+        /* save raw line before parse mutates it */
+        char save[INPUT_SIZE];
+        strncpy(save, input, sizeof(save));
+        save[sizeof(save) - 1] = '\0';
+        char *nl = strchr(save, '\n');
+        if (nl)
+            *nl = '\0';
 
         if (parse_input(input, &pl) < 0)
             continue;
@@ -54,13 +100,18 @@ int main(void)
         if (strcmp(pl.cmds[0].argv[0], "exit") == 0)
             break;
 
+        history_add(&hist, save);
+
         if (pl.num_cmds == 1 && is_builtin(pl.cmds[0].argv[0])) {
-            run_builtin(&pl.cmds[0]);
+            last_status = run_builtin(&pl.cmds[0], &hist, last_status);
             continue;
         }
 
-        execute_pipeline(&pl);
+        last_status = execute_pipeline(&pl);
+        if (last_status < 0)
+            last_status = 1;
     }
 
-    return 0;
+    history_free(&hist);
+    return last_status;
 }
